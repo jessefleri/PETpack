511bce82ebd01bd7a533edb4671c8beef159c396
Jesse Fleri <jessefleri@gmail.com>
2017-12-06 06:05:00 +0000

    Replace web_mapper.py

--- code/web_mapper.py
+++ code/web_mapper.py
@@ -13,24 +13,24 @@ import json
 import vincent
 import pandas as pd
 
-df = pd.read_excel(r'climate_data.xlsx',header=0)
+df = pd.read_excel(r'climate_data.xlsx',header=0)                     # Read climate data csv into pandas dataframe
 
-web = folium.Map(
+web = folium.Map(                                                     # Create empty map and zoom to particular location
     location=[39.7370, -110.8338],
     zoom_start=7,
     tiles='Stamen Terrain')
 
-for index, row in df.iterrows():
-    tem = row[1:13]
-    model = pet.ET1(tem,df.iloc[index,14],2000)
-    scatter = vincent.Line(model, width=400, height=200)
+for index, row in df.iterrows():                                      # Loop over dateframe 
+    tem = row[1:13]                                                   # Monthly temperature values
+    model = pet.ET1(tem,df.iloc[index,14],2000)                       # Run model with latitude pulled from dataframe row
+    scatter = vincent.Line(model, width=400, height=200)              # Creater scatterplot
     scatter.axis_titles(x='Month', y='Potential ET (mm)')
     scatter.legend(title='Models')
     scatter_dict = json.loads(scatter.to_json())
-    popup = folium.Popup(max_width=650)
-    folium.Vega(scatter_dict, height=250, width=525).add_to(popup)
-    folium.CircleMarker([df.iloc[index,14], df.iloc[index,15]],
+    popup = folium.Popup(max_width=650)                               # Create popup space
+    folium.Vega(scatter_dict, height=250, width=525).add_to(popup)    # Fill popup space with chart
+    folium.CircleMarker([df.iloc[index,14], df.iloc[index,15]],       # Add to map before iterating again
                         color='crimson', fill=True, fill_color='crimson',
                         popup=popup).add_to(web)
 
-web.save('pet-map.html')
+web.save('pet-map.html')                                              # Save map
